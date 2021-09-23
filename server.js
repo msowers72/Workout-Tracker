b2628367114aaa93b9ad1ff5d3f5e848e9011671
Mike Sowers <msowers72@yahoo.com>
2021-09-23 14:33:28 -0400

    inital file setup

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
